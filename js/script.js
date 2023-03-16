93165c3147bfe55d445d1ef4ec4b61764f63e0fa
Andika Dwiki <andika0106dwiki@gmail.com>
2023-03-16 13:43:55 +0700

    add main section

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ const menuNav = document.querySelector(".menu");
 menuBar.addEventListener("click", () => {
   menuNav.classList.toggle("menu-active");
 });
+
+let mybutton = document.getElementById("toTop");
+window.addEventListener("scroll", scrollFunction);
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
